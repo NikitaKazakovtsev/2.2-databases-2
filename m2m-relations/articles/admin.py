a8597c2326abbc58ae6cc8cbef534b158a2bb85e
Kazakovtsev Nikita <nikita_kazakovtsev@bk.ru>
2023-06-21 16:27:25 +0300

    first commit

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib import admin
+from .models import Article, Tag, Scope
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    class Meta:
+        ordering = '-tag'
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if self.deleted_forms and self._should_delete_form(form):
+                continue
+            if form.cleaned_data.get('is_main'):
+                count += 1
+        if count > 1:
+            raise ValidationError('Основной раздел, - может быть только один.')
+        elif count == 0:
+            raise ValidationError('Укажите основной раздел')    
+        return super().clean()  
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline, ]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']  
